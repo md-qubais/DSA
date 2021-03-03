fabe647907ed0454153282fbe9565f5b3d2eee1e
Mohammed Qubaisuddin <shareefhussain733@gmail.com>
2021-03-03 22:15:02 +0530

    wiggle sort

---
+++ wiggle_sort.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+#define IOS ios_base::sync_with_stdio(false);  cin.tie(NULL)
+#define int long long
+#define endl "\n"
+#define qubais_judge freopen("input.txt","r",stdin); freopen("output.txt","w",stdout)
+using namespace std;
+void wiggleSort(vector<int>& nums){
+	
+}
+int32_t main(){
+	qubais_judge;
+	IOS;
+	int n;
+	cin>>n;
+	vector<int> arr(n);
+	for(int i=0;i<n;i++){
+		cin>>arr[i];
+	}
+	wiggleSort(arr);
+	for(auto it:arr){
+		cout<<it<<" ";
+	}
+	return 0;
+}
